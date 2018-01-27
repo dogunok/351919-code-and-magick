4b7ca67385f0b8f83c24e4bbd4bbc6a7b921ee04
dogunok <spam-system.s@yandex.ru>
2018-01-27 21:16:39 +0300

    пофиксил пробелы

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,3 @@
-
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -6,48 +5,26 @@ var CLOUD_Y = 10;
 var GAP = 10;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
-var BAR_HEIGHT =150   ;
-
+var BAR_HEIGHT =150;
 //Общая функция для создания облака
 var renderCloud = function (ctx,x ,y, color) {
-  ctx.fillStyle = color ;
+  ctx.fillStyle = color;
   ctx.fillRect (x,y,CLOUD_WIDTH,CLOUD_HEIGHT);
 };
-
 window.renderStatistics = function(ctx, names, times) {
 //создаем 2 облака
   renderCloud(ctx, CLOUD_X+GAP, CLOUD_Y+GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y,'#ffffff');
-
-
 ctx.fillStyle = '#000';
 for (var i = 0;i < names.length;i++){
   ctx.fillText(names[i], CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i,BAR_HEIGHT+GAP*2+BAR_GAP );
   ctx.fillText(Math.round(times[i]), CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i,BAR_GAP-GAP );
   ctx.fillRect(CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i, BAR_GAP,BAR_WIDTH, BAR_HEIGHT);
-
-
     switch(names[i]){
-
       case 'Вы':
        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
       break;
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-  };
+ };
 
