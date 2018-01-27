f9c494c5ba92d48386cb3168cd7ed175e2e9891b
dogunok <spam-system.s@yandex.ru>
2018-01-27 21:32:48 +0300

    Исправил ошибки

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,13 @@
-var CLOUD_WIDTH = 420;
+var CLOUD_WIDTH;
+CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var BAR_GAP = 50;
-var BAR_WIDTH = 40;
+var BAR_GAP =50;
+var BAR_WIDTH =40;
 var BAR_HEIGHT =150;
-//Общая функция для создания облака
-var renderCloud = function (ctx,x ,y, color) {
+var renderCloud = function (ctx,x ,y, color){
   ctx.fillStyle = color;
   ctx.fillRect (x,y,CLOUD_WIDTH,CLOUD_HEIGHT);
 };
@@ -17,8 +17,8 @@ window.renderStatistics = function(ctx, names, times) {
   renderCloud(ctx, CLOUD_X, CLOUD_Y,'#ffffff');
 ctx.fillStyle = '#000';
 for (var i = 0;i < names.length;i++){
-  ctx.fillText(names[i], CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i,BAR_HEIGHT+GAP*2+BAR_GAP );
-  ctx.fillText(Math.round(times[i]), CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i,BAR_GAP-GAP );
+  ctx.fillText(names[i], CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i,BAR_HEIGHT+GAP*2+BAR_GAP);
+  ctx.fillText(Math.round(times[i]), CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i,BAR_GAP-GAP);
   ctx.fillRect(CLOUD_X+BAR_WIDTH +(BAR_GAP+BAR_WIDTH)*i, BAR_GAP,BAR_WIDTH, BAR_HEIGHT);
     switch(names[i]){
       case 'Вы':
