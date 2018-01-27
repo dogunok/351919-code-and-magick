ea2d5efd236b8bf7aadefe7b9ab342893610753d
dogunok <spam-system.s@yandex.ru>
2018-01-28 00:00:29 +0300

    Исправление ошибок v1.2

--- js/stat.js
+++ js/stat.js
@@ -9,21 +9,21 @@ var barWidth = 40;
 var barHeight = 150;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect (x,y,cloudWidth,cloudHeight);
+  ctx.fillRect (x, y, cloudWidth, cloudHeight);
 };
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
 
   renderCloud(ctx, cloudX + gap, cloudY + gap, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, cloudX, cloudY,'#ffffff');
-ctx.fillStyle = '#000';
-for (var i = 0; i < names.length; i++) {
-  ctx.fillText(names[i], cloudX + barWidth + (barGap + barWidth) * i, barHeight + gap * 2 + barGap);
-  ctx.fillText(Math.round(times[i]), cloudX + barWidth + (barGap + barWidth) * i, barGap - gap);
-  ctx.fillRect(cloudX + barWidth + (barGap + barWidth) * i, barGap, barWidth, barHeight);
-    switch(names[i]) {
+  renderCloud(ctx, cloudX, cloudY, '#ffffff');
+  ctx.fillStyle = '#000';
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillText(names[i], cloudX + barWidth + (barGap + barWidth) * i, barHeight + gap * 2 + barGap);
+    ctx.fillText(Math.round(times[i]), cloudX + barWidth + (barGap + barWidth) * i, barGap - gap);
+    ctx.fillRect(cloudX + barWidth + (barGap + barWidth) * i, barGap, barWidth, barHeight);
+    switch (names[i]) {
       case 'Вы':
-       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      break;
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    break;
     }
   }
 };
