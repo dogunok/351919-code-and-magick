5dc2a5d56ff20d5a0c715fae5fc7146bcad9bf16
dogunok <spam-system.s@yandex.ru>
2018-01-29 00:24:06 +0300

    Добавил функцию для описания текста

--- js/stat.js
+++ js/stat.js
@@ -7,6 +7,8 @@ var gap = 10;
 var barGap = 50;
 var barWidth = 40;
 var barHeight = 150;
+var text1 = 'Ура вы победили!';
+var text2 = 'Список результатов:';
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, cloudWidth, cloudHeight);
@@ -20,15 +22,19 @@ var getMaxElement = function (arr) {
   }
   return maxElement;
 };
-function getRandom() {
+var getRandom = function () {
   return Math.random();
-}
+};
+var placeText = function (ctx, text, x, y, size, color) {
+  ctx.font = size;
+  ctx.fillStyle = color;
+  ctx.fillText(text, x, y);
+};
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, cloudX + gap, cloudY + gap, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, cloudX, cloudY, '#ffffff');
   ctx.fillStyle = '#083689';
   var maxTime = getMaxElement(times);
-
   for (var i = 0; i < names.length; i++) {
     var fixedRandom = getRandom().toFixed(1);
     if (names[i] === 'Вы') {
@@ -37,11 +43,10 @@ window.renderStatistics = function (ctx, names, times) {
       ctx.fillStyle = 'rgba(8,54,137, ' + fixedRandom + ')';
     }
     ctx.fillRect(cloudX + barWidth + (barGap + barWidth) * i, barGap * 2 + (barHeight - ((barHeight * times[i]) / maxTime)), barWidth, (barHeight * times[i]) / maxTime);
-    ctx.fillStyle = '#000';
     ctx.fillText(names[i], cloudX + barWidth + (barGap + barWidth) * i, barHeight + gap * 2 + barGap * 2);
     ctx.fillText(Math.round(times[i]), cloudX + barWidth + (barGap + barWidth) * i, barGap * 2 - gap + (barHeight - ((barHeight * times[i]) / maxTime)));
     ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', 120, 50);
-    ctx.fillText('Список результатов:', 120, 70);
+    placeText(ctx, text1, 120, 50, '16px PT Mono', '#000');
+    placeText(ctx, text2, 120, 70, '16px PT Mono', '#000');
   }
 };
