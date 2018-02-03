99f3a8a032b2ccf0031312dbd1708d84c4328e13
dogunok <spam-system.s@yandex.ru>
2018-02-03 11:26:01 +0300

    Создал цикл создания определенного колличества массива

--- js/setup.js
+++ js/setup.js
@@ -7,36 +7,28 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_FAMILY = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var lotWizard = 3;
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 var wizards = [
   {
-    name: WIZARD_NAMES[getRandom(0, 7)],
-    family: WIZARD_FAMILY[getRandom(0, 7)],
-    coatColor: WIZARD_COAT_COLOR[getRandom(0, 5)],
-    eyesColor: WIZARD_EYES_COLOR[getRandom(0, 4)]
-  },
-  {
-    name: WIZARD_NAMES[getRandom(0, 7)],
-    family: WIZARD_FAMILY[getRandom(0, 7)],
-    coatColor: WIZARD_COAT_COLOR[getRandom(0, 5)],
-    eyesColor: WIZARD_EYES_COLOR[getRandom(0, 4)]
-  },
-  {
-    name: WIZARD_NAMES[getRandom(0, 7)],
-    family: WIZARD_FAMILY[getRandom(0, 7)],
-    coatColor: WIZARD_COAT_COLOR[getRandom(0, 5)],
-    eyesColor: WIZARD_EYES_COLOR[getRandom(0, 4)]
-  },
-  {
-    name: WIZARD_NAMES[getRandom(0, 7)],
-    family: WIZARD_FAMILY[getRandom(0, 7)],
-    coatColor: WIZARD_COAT_COLOR[getRandom(0, 5)],
-    eyesColor: WIZARD_EYES_COLOR[getRandom(0, 4)]
+    name: WIZARD_NAMES[getRandom(0, WIZARD_NAMES.length - 1)],
+    family: WIZARD_FAMILY[getRandom(0, WIZARD_FAMILY.length - 1)],
+    coatColor: WIZARD_COAT_COLOR[getRandom(0, WIZARD_COAT_COLOR.length - 1)],
+    eyesColor: WIZARD_EYES_COLOR[getRandom(0, WIZARD_EYES_COLOR.length - 1)]
   }
 
 ];
+
+for (var j = 0; j < lotWizard; j++) {
+  wizards.push({
+    name: WIZARD_NAMES[getRandom(0, WIZARD_NAMES.length - 1)],
+    family: WIZARD_FAMILY[getRandom(0, WIZARD_FAMILY.length - 1)],
+    coatColor: WIZARD_COAT_COLOR[getRandom(0, WIZARD_COAT_COLOR.length - 1)],
+    eyesColor: WIZARD_EYES_COLOR[getRandom(0, WIZARD_EYES_COLOR.length - 1)]
+  });
+}
 
 
 var renderWizard = function (wizard) {
